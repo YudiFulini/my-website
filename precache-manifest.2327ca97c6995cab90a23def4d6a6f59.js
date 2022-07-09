ad1ede5e7356028e7bbbff34579902354bf82465
YudiFulini <felipefulini@msn.com>
2022-07-08 21:12:08 -0300

    Updates

---
+++ precache-manifest.2327ca97c6995cab90a23def4d6a6f59.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d1c34c1f6a54856f102",
+    "url": "/mypage/static/css/main.974eaebc.chunk.css"
+  },
+  {
+    "revision": "0d1c34c1f6a54856f102",
+    "url": "/mypage/static/js/main.4745dd63.chunk.js"
+  },
+  {
+    "revision": "1d0eb0fb2dd27e69d724",
+    "url": "/mypage/static/js/runtime~main.4c2b4ee8.js"
+  },
+  {
+    "revision": "760b254c9e716e45eead",
+    "url": "/mypage/static/js/2.7a848a05.chunk.js"
+  },
+  {
+    "revision": "bf06e3bfde43810540ba219f38749fd6",
+    "url": "/mypage/static/media/page-icon.bf06e3bf.png"
+  },
+  {
+    "revision": "3dd527b675131beb757a745a57aa4817",
+    "url": "/mypage/static/media/books-icon.3dd527b6.png"
+  },
+  {
+    "revision": "c9ac9c1a4b7b76db5e8a3764c869a782",
+    "url": "/mypage/static/media/profile-icon.c9ac9c1a.png"
+  },
+  {
+    "revision": "821fa01d971ed933d64b6412f80ae1de",
+    "url": "/mypage/static/media/software-development.821fa01d.png"
+  },
+  {
+    "revision": "2b7a1a064d4598e17b601d169c5bd092",
+    "url": "/mypage/static/media/my-photo.2b7a1a06.jpg"
+  },
+  {
+    "revision": "25b694ba78065655dcee6237019aceda",
+    "url": "/mypage/static/media/html5-icon.25b694ba.png"
+  },
+  {
+    "revision": "b848d5d9bb221592064de0f356f61676",
+    "url": "/mypage/static/media/css3-icon.b848d5d9.png"
+  },
+  {
+    "revision": "de303a4b45071fde56699d2ccb5eb60e",
+    "url": "/mypage/static/media/react-icon.de303a4b.png"
+  },
+  {
+    "revision": "4ab61498675e9df9c36b0fdaf34e4655",
+    "url": "/mypage/static/media/bootstrap-icon.4ab61498.png"
+  },
+  {
+    "revision": "923523c6771b3c766c28016406a4dedf",
+    "url": "/mypage/static/media/arduino-icon.923523c6.png"
+  },
+  {
+    "revision": "ebf185dbe0e75a688f66e86dbbb21c44",
+    "url": "/mypage/static/media/matlab-icon.ebf185db.png"
+  },
+  {
+    "revision": "816f655be30e7447d6bf8da9540e3a40",
+    "url": "/mypage/static/media/brazil-icon.816f655b.png"
+  },
+  {
+    "revision": "2822f08635ba028c6928d633a4416951",
+    "url": "/mypage/static/media/usa-icon.2822f086.png"
+  },
+  {
+    "revision": "abfcc8e287fc4a55d1db32ab3b78a8db",
+    "url": "/mypage/static/media/anime-img.abfcc8e2.jpg"
+  },
+  {
+    "revision": "4f6b0ad10d7822aa58cde14c2e315201",
+    "url": "/mypage/static/media/game-img.4f6b0ad1.jpg"
+  },
+  {
+    "revision": "1f0f03befdd2f059bfc0582bb79381d0",
+    "url": "/mypage/static/media/webdev-img.1f0f03be.jpg"
+  },
+  {
+    "revision": "699f30af86c77d832e657a14b6c1f57f",
+    "url": "/mypage/static/media/photo-img.699f30af.jpg"
+  },
+  {
+    "revision": "26330e02e36fab90d74167378d2f49d3",
+    "url": "/mypage/static/media/travel-img.26330e02.jpeg"
+  },
+  {
+    "revision": "c9a398329bb904544bdbea32b963e2fc",
+    "url": "/mypage/index.html"
+  }
+];

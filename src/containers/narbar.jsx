21accd44ef34ecb45289f2c6753c0e06ce09d5d5
YudiFulini <felipefulini@msn.com>
2022-07-13 17:50:28 -0300

    changes on Navbar

--- src/containers/narbar.jsx
+++ src/containers/narbar.jsx
@@ -9,18 +9,22 @@ import EngineIcon from './icons/software-development.png';
 
 import {Link} from 'react-router-dom'
 
-const linkStyle = {
+const linkTitleStyle = {
   textDecoration: "none",
   color: 'white',
+};
+const linkStyle = {
+  textDecoration: "none",
+  color: 'rgba(240, 240, 240, 0.8)',
 };
 
 const navbar = ()=>{
   return (
     <>
-      <Navbar className="py-2 border-bottom border-light fixed-top"  bg="dark" expand="lg" variant="dark" >
+      <Navbar className="py-2 border-bottom border-light fixed-top"  bg="dark" expand="md" variant="dark" >
         <Container fluid="lg">
           <Navbar.Brand className="d-flex align-items-center">
-            <Link to="/" style={linkStyle}>
+            <Link to="/" style={linkTitleStyle}>
             <img src={PageLogo} width="30" height="30" className="d-inline-block align-top" />
             <p className="d-inline-flex font-8bit-navbar my-0 mx-3">Fuyuni</p>
             </Link>
@@ -29,21 +33,21 @@ const navbar = ()=>{
           <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
             <Nav>
               <Nav.Link>
-                <Link to="/"  style={linkStyle}>
-                  <img src={ProfileIcon} width="23" height="23" className="d-inline-block align-top"/>
-                  {' '} About Me
+                <Link to="/" className="d-flex justify-content-end" style={linkStyle}>
+                  {'About Me'}
+                  <img src={ProfileIcon} width="23" height="23" className="mx-1 d-inline-block align-top"/>
                 </Link> 
               </Nav.Link>
               <Nav.Link>
-                <Link to="/projects"  style={linkStyle}>
-                  <img src={EngineIcon} width="23" height="23" className="d-inline-block align-top"/>
-                  {' '} Projects 
+                <Link to="/projects" className="d-flex justify-content-end" style={linkStyle}>
+                  {'Projects'}
+                  <img src={EngineIcon} width="23" height="23" className="mx-1  d-inline-block align-top"/>
                 </Link>
               </Nav.Link>
               <Nav.Link > 
-              <Link to="/studies"  style={linkStyle}>
-                <img src={BookIcon} width="23" height="23" className="d-inline-block align-top"/>
-                {' '} Studies
+              <Link to="/studies" className="d-flex justify-content-end" style={linkStyle}>
+                {'Studies'}
+                <img src={BookIcon} width="23" height="23" className="mx-1 d-inline-block align-top"/>
               </Link>
               </Nav.Link>
             </Nav>
